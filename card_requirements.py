dea228efe937801c094039683d37031f5d4cd6f8
EKTurduckin <50594726+EKTurduckin@users.noreply.github.com>
2025-03-11 22:55:13 -0700

    Update card_requirements.py

--- card_requirements.py
+++ card_requirements.py
@@ -23,6 +23,22 @@ def fabrary_list_import(file):
             
     return card_list
 
+def total_cards(import_file):
+    output = {}
+
+    for record in import_file:
+        if not record[2]:
+            pitch = None
+        else:
+            pitch = record[2]
+
+        if (record[1], pitch) not in output:
+            output[(record[1], pitch)] = record[0]
+        else:
+            output[(record[1], pitch)] += record[0]
+    
+    return output
+
 def get_card_library():
     library = requests.get("https://raw.githubusercontent.com/the-fab-cube/flesh-and-blood-cards/refs/heads/develop/json/english/card-flattened.json").json()
     
@@ -34,17 +50,39 @@ def get_card_library():
     
     return library
 
+def get_sets(import_file):
+    output = {}
+    card_library = get_card_library()
+
+    for record in import_file:
+        sets = []
+        for entry in card_library:
+            if record[1].strip() == entry["name"] and record[3] == entry["pitch"]:
+                if entry["set_id"] not in sets:
+                    sets.append(entry["set_id"])
+        
+        if record[3] == 1:
+            pitch = 'red'
+        if record[3] == 2:
+            pitch = 'yellow'
+        if record[3] == 3:
+            pitch = 'blue'
+        if not record[3]:
+            pitch = None
+
+        if(record[1], pitch) not in output:
+            output[(record[1], pitch)] = ', '.join(sets)
+
+    return output
+
+
 battle_box = fabrary_list_import("Battle Box.txt")
-card_library = get_card_library()
+battle_box_summed = total_cards(battle_box)
+battle_box_sets = get_sets(battle_box)
 
-for card in battle_box:
-    sets = []
-    for entry in card_library:
-        if card[1].strip() == entry["name"] and card[3] == entry["pitch"]:
-            if entry["set_id"] not in sets:
-                sets.append(entry["set_id"])
-    card.append(sets)
+sum = pd.DataFrame.from_dict(battle_box_summed, orient="index",columns=["qty"])
+set = pd.DataFrame.from_dict(battle_box_sets, orient="index",columns=["sets"])
 
-battle_box_df = pd.DataFrame().from_records(battle_box, columns=["qty","name","pitch_name","pitch_value","sets"])
-
-battle_box_df.to_clipboard(index=False)
+final = pd.concat([sum, set], axis=1)
+final = pd.concat([final, pd.DataFrame(final.index.to_list(), index=final.index, columns=["card", "pitch"])], axis=1)
+final.loc[:,["card","pitch","qty","sets"]].to_clipboard(index=False)
